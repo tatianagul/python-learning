522d367d09cabca2f352f34325d5a1ce0d9e98b5
tatianagul <tyana.evt@gmail.com>
2025-12-19 11:36:35 +0300

    Add command registry

---
+++ oop/command_engine/registry.py
@@ -0,0 +1,12 @@
+COMMAND_REGISTRY = {}
+
+
+def register_command(name, func, description=""):
+    COMMAND_REGISTRY[name] = {
+        "function": func,
+        "description": description
+    }
+
+
+def get_commands():
+    return COMMAND_REGISTRY
