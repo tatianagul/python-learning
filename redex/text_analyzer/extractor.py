a0177ecb66cc13fc5470a08eb87f601c558371bf
tatianagul <tyana.evt@gmail.com>
2026-01-02 22:37:02 +0300

    add phone extraction logic

--- redex/text_analyzer/extractor.py
+++ redex/text_analyzer/extractor.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import re
 
-from patterns import email_pattern
+from patterns import email_pattern, phone_pattern
 
 
 def extract_emails(text_file: Path):
@@ -9,3 +9,12 @@ def extract_emails(text_file: Path):
         content = file.read()
         email_list = set(re.findall(email_pattern, content))
     return sorted(email_list)
+
+
+def extract_phones(text_file: Path):
+    phone_list = set()
+    with text_file.open("r", encoding="utf-8") as file:
+        for line in file:
+            matches = re.findall(phone_pattern, line)
+            phone_list.update(matches)
+    return sorted(phone_list)
--- redex/text_analyzer/main.py
+++ redex/text_analyzer/main.py
@@ -1,8 +1,10 @@
 from pathlib import Path
-from extractor import extract_emails
+from extractor import extract_emails, extract_phones
 
 
 text_file = Path("data/sample.txt")
 
 
 print(extract_emails(text_file))
+print()
+print(extract_phones(text_file))
